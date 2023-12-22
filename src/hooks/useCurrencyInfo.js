72d0ced713069e439c832ed8e35ec0770d9f7228
Ayush Maurya <amaurya5556@gmail.com>
2023-12-23 02:31:33 +0530

    Creat the initial currency converter application

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,25 @@
+// we keep file this name as a js extension
+// because it is generally a good practice since hook in most cases return js
+// jsx is preferred is you are returnig a lot jsx value
+
+// hooks name genrally start with use to have considtency with
+// other react hooks
+
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((response) => response.json())
+        .then((currencyObject) => setData(currencyObject[currency]));
+    }, [currency]);
+
+    console.log(data);
+
+    return data;
+}
+
+export default useCurrencyInfo;
